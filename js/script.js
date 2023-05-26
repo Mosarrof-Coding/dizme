4111ec4947c2abc60c8a7ce07272f1bf1fe10642
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-05-26 10:37:22 +0600

    yes

--- js/script.js
+++ js/script.js
@@ -68,3 +68,17 @@ cursorScale.forEach(link => {
     cursor.classList.remove('grow-small');
   });
 });
+
+// JS.tilt-----------------
+$('.js-tilt').tilt({
+  maxTilt:20,
+  perspective:    5000,   // Transform perspective, the lower the more extreme the tilt gets.
+  easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  scale:          1.05,      // 2 = 200%, 1.5 = 150%, etc..
+  speed:          300,    // Speed of the enter/exit transition.
+  transition:     true,   // Set a transition on enter/exit.
+  disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+  reset:          true,   // If the tilt effect has to be reset on exit.
+  glare:          true,  // Enables glare effect
+  maxGlare:       1       // From 0 - 1.
+})
